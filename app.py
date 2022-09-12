28b6325432d89aba5c4144944cc6ce9e43aa7c03
Muzaffer Şenkal <muzaffer727@hotmail.com>
2022-09-12 23:46:38 +0100

    updated

--- app.py
+++ app.py
@@ -4,62 +4,98 @@ import pandas as pd
 from io import StringIO
 import zipfile
 import uuid
+import py7zr
 
+def get_only_folders(path):
+    
+    files = os.listdir(path)
+    return list(filter(lambda x: os.path.isdir(os.path.join(path,x)), files))
+
+def find_first_int(text): # to find block
+    for i, c in enumerate(text):
+        if c.isdigit():
+            return i
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
      # To read file as bytes:
     uniq = uuid.uuid4().hex
-    name = uploaded_file.name.split(".")[0] 
-    with zipfile.ZipFile(uploaded_file,"r") as z:
-        z.extractall(f"./uploaded/{name}_{uniq}/")
+    new_folder = f"./uploaded/{uniq}/"
+    #name = uploaded_file.name.split(".")[0] 
+
+    if uploaded_file.name.split(".")[-1]  == "7z":
+        with py7zr.SevenZipFile(uploaded_file, mode='r') as z:
+            z.extractall(new_folder)
+    else:
+        with zipfile.ZipFile(uploaded_file,"r") as z:
+            z.extractall(new_folder)
+
+    
 
     dataArray=[]
-    for root, dirnames, filenames in os.walk(f"./uploaded/{name}_{uniq}/"):
-        data = dict()
-        if "user.csv" in filenames:
-            folderName = root.split("/")
-            data["participant"] = name
-            data["Des-row"] = int(folderName[-1][8])
-            
-            # location
-            df_temp = pd.read_csv(os.path.join(root,"location choice.csv")).to_dict()
-            data["Zone Elapsed Time"] = df_temp["Elapsed Time"][0]
-            data["Zone"] = df_temp["Zone"][0]
-            data["Postcode"] = df_temp["Postcode"][0]
-            data["Selected Location"] = df_temp["Selected Location"][0]
-            # vehicle
-            df_temp = pd.read_csv(os.path.join(root,"vehicle choice.csv")).to_dict()
-            data["Vehicle Elapsed Time"] = df_temp["Elapsed Time"][0]
-            data["Vehicle Choice"] = df_temp["Vehicle Choice"][0]
-            # payment
-            df_temp = pd.read_csv(os.path.join(root,"payment.csv")).to_dict()
-            data["Payment Elapsed Time"] = df_temp["Elapsed Time"][0]
-            data["Payment Option"] = df_temp["Payment Option"][0]
-            
-            # options
-            df_temp = pd.read_csv(os.path.join(root,"options.csv")).to_dict(orient="records")
-            
-            data["Chat Elapsed Time"] = df_temp[0]["Elapsed Time"]
-            data["Chat With Operator"] = df_temp[0]["Selected State"]
-            
-            data["CD Elapsed Time"] = df_temp[1]["Elapsed Time"]
-            data["Change Destination"] = df_temp[1]["Selected State"]
-            data["RTD Elapsed Time"] = df_temp[2]["Elapsed Time"]
-            data["Real-Time Display"] = df_temp[2]["Selected State"]
-            
-            if len(df_temp) > 3:
-                data["Chat2 Elapsed Time"] = df_temp[3]["Elapsed Time"]
-                data["Chat2 With Operator"] = df_temp[3]["Selected State"]
-            else:
-                data["Chat2 Elapsed Time"] = ""
-                data["Chat2 With Operator"] = ""
-            dataArray.append(data)
+
+    experiments = get_only_folders(new_folder)
+
+    for exp in experiments:
+        experiment_folder = os.path.join(new_folder, exp)
+        desRows = get_only_folders(experiment_folder)
+        desRows = list(filter(lambda x:x.startswith("Des_row"), desRows))
+    
+        
+
+        if  len(desRows) == 8:
+    
+            for d in desRows:
+                root = os.path.join(experiment_folder, d)
+                data = dict()
+                
+                if "user.csv" in os.listdir(root):
+                    exSplit = exp.split()
+                    data["fullName"] = exp
+                    data["participant"] = exSplit[0]+ " " + exSplit[1] + exSplit[2]
+                    
+                    blockName = "".join(exSplit[-2:])
+                    blockIntegerIndex = find_first_int(blockName)
+                    blockName = blockName[blockIntegerIndex:]
+                    data["Des-row"] = int(d.split()[-1].split("__")[0])
+                    data["block"] = blockName
+
+                    # location
+                    df_temp = pd.read_csv(os.path.join(root,"location choice.csv")).to_dict()
+                    data["Zone Elapsed Time"] = df_temp["Elapsed Time"][0]
+                    data["Zone"] = df_temp["Zone"][0]
+                    data["Postcode"] = df_temp["Postcode"][0]
+                    data["Selected Location"] = df_temp["Selected Location"][0]
+                    # vehicle
+                    df_temp = pd.read_csv(os.path.join(root,"vehicle choice.csv")).to_dict()
+                    data["Vehicle Elapsed Time"] = df_temp["Elapsed Time"][0]
+                    data["Vehicle Choice"] = df_temp["Vehicle Choice"][0]
+                    # payment
+                    df_temp = pd.read_csv(os.path.join(root,"payment.csv")).to_dict()
+
+                    data["Payment Elapsed Time"] = df_temp["Elapsed Time"][0]
+                    data["Payment Option"] = df_temp["Payment Option"][0]
+
+                    # options
+                    df_temp = pd.read_csv(os.path.join(root,"options.csv")).to_dict(orient="records")
+
+                    options = []
+                    for opt in df_temp:
+
+                        if opt["Option"] in options:
+                            optName = opt["Option"]+ " Second"
+                            data[optName] = opt["Selected State"]
+                            data[optName + " Elapsed Time"] = opt["Elapsed Time"]
+                        else:
+                            optName = opt["Option"]
+                            data[optName] = opt["Selected State"]
+                            data[optName + " Elapsed Time"] = opt["Elapsed Time"]
+                        options.append(opt["Option"])
+                    dataArray.append(data)
     
     df = pd.DataFrame(dataArray)
-    df_new = df.sort_values(by=["Des-row"]).reset_index().drop(columns=["index"])
-    name = df_new.to_dict()["participant"][0]
-    final_filename = name+"_final_data.xlsx"
+    df_new = df.sort_values(by=["fullName","Des-row"]).reset_index().drop(columns=["index"])
+    final_filename = "final_data.xlsx"
     df_new.to_excel(final_filename,index=False)
 
     with open(final_filename, 'rb') as my_file:
